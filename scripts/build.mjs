e3cff0544ff2a99e2a507048b7b0c2efb27d5355
Teffen Ellis <teffen@nirri.us>
2022-06-09 17:46:22 -0400

    Remove previous builds. Remove unused css.

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,9 +1,23 @@
 import esbuild from 'esbuild'
 import svgrPlugin from 'esbuild-plugin-svgr'
 import FastGlob from 'fast-glob'
+import { constants } from 'node:fs'
+import * as fs from 'node:fs/promises'
 import { projectPath } from '../paths.mjs'
 
 const watch = process.argv.some((arg) => arg === '--watch')
+
+async function cleanDir(filePath) {
+  const present = await fs
+    .access(filePath, constants.F_OK)
+    .then(() => true)
+    .catch(() => false)
+
+  if (!present) return
+
+  console.log('Cleaning...', filePath)
+  return fs.rm(filePath, { recursive: true })
+}
 
 /** @type {import(esbuild.BuildOptions)} */
 const commonEsbuildConfig = {
@@ -15,11 +29,13 @@ const commonEsbuildConfig = {
 
 async function buildBrowser() {
   console.log('[Browser]', 'Building...')
+  const outdir = projectPath('dist')
+  await cleanDir(outdir)
 
   await esbuild.build({
     ...commonEsbuildConfig,
     entryPoints: [projectPath('packages', 'browser', 'src/index.tsx')],
-    outdir: projectPath('dist'),
+    outdir,
   })
 
   console.log('[Browser]', 'Built!')
@@ -28,14 +44,24 @@ async function buildBrowser() {
 async function buildWorker() {
   console.log('[Worker]', 'Building...')
   const entryPoints = await FastGlob(projectPath('packages', 'worker', 'functions', '**', '*.{ts,tsx,mts}'))
+  const outdir = projectPath('functions')
 
-  await esbuild.build({
+  await cleanDir(outdir)
+
+  const result = await esbuild.build({
     ...commonEsbuildConfig,
-    // external: ['*.css'],
     format: 'esm',
     entryPoints: entryPoints,
-    outdir: projectPath('functions'),
+    metafile: true,
+    outdir,
   })
+
+  const cssFiles = Object.keys(result.metafile.outputs).filter((outfile) => outfile.endsWith('.css'))
+  await Promise.all(
+    cssFiles.map((cssFilePath) => {
+      return fs.rm(cssFilePath)
+    })
+  )
 
   console.log('[Worker]', 'Built!')
 }
